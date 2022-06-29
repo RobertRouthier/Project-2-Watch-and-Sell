01df64d3178f836ea50c4388415af2d17b1a65a3
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-06-28 19:46:45 -0700

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// Import the Sequelize constructor from the library
+
+const Sequelize = require('sequelize');
+
+// Create a connection to our db, create a ".env" file for your MySQL information 
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+      port: process.env.PORT || 8889
+    });
+
+module.exports = sequelize;
