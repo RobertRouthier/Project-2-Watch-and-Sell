52c37edbe8520f2856867917e6b96c38249b618b
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-06-29 22:19:24 -0700

    added jest.js function

---
+++ test/sum.js
@@ -0,0 +1,8 @@
+// Hypothetical function that adds two numbers - Jest.js
+
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
+  
+  
---
+++ test/sum.test.js
@@ -0,0 +1,11 @@
+//  Actual function test - Jest.js
+
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 * 2 to equal 6', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
