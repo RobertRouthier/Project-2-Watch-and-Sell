4a86c1df5a7c7286445b16bf3d157b2054df71b1
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-06-30 20:46:30 -0700

    added avaiRoutes.js

---
+++ controllers/availRoutes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Watch, User, Like, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all Watches for availability.handlebars
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    Watch.findAll({
+        where: {
+          user_id: req.session.user_id
+          },
+        attributes: [
+            'id',
+            'name',
+            'model',
+            'price',
+            'condition',
+            'location',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE watch.id = like.watch_id)'), 'like_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'watch_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+
+      // Render for availability.handlebar
+        .then(dbWatchData => {
+          const watches = dbWatchData.map(watch => watch.get({ plain: true }));
+          res.render('availability', {watches, loggedIn: true});
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+// GET Watch by ID - watch-container.handlebars
+router.get('/watch/:id', (req, res) => {
+    Watch.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'name',
+        'model',
+        'price',
+        'condition',
+        'location',
+        [sequelize.literal('(SELECT COUNT(*) FROM like WHERE watch.id = like.watch_id)'), 'like_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'watch_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbWatchData => {
+        if (!dbWatchData) {
+          res.status(404).json({ message: 'Watch NOT found with this id' });
+          return;
+        }
+
+        const watch = dbWatchData.get({ plain: true });
+
+        // Render for watch-container.handlebar
+        res.render('watch-container', {
+            watch,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+  module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
+const availRoutes = require('./availRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/availabilty', availRoutes);
 
 router.use((req, res) => {
     res.send("<h1>Wrong Route!</h1>");
