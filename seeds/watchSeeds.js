4a27e8123074930a3953ac41d0410de23a3a1799
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-06-30 22:02:31 -0700

    updated seeds

--- seeds/watchSeeds.js
+++ seeds/watchSeeds.js
@@ -2,42 +2,49 @@ const { Watch } = require('../models');
 
 const watchData = [
     {
-        name: 'Rolex Submariner',
-        model: '126600LN',
+        name: 'Rolex Submariner DATE',
+        model: '126610LN',
+        // Data Truncated ERROR
+        // price: '$20,000',
+        // condition: 'Good',
+        // location: 'USA',
         created_at: new Date(),
     },
-    // {
-    //     name: 'Rolex Submariner',
-    //     model: '126600LN',
-    //     price: '$20,000',
-    //     condition: 'Good',
-    //     location: 'USA',
-    //     created_at: new Date(),
-    // },
-    // {
-    //     name: 'Rolex Submariner',
-    //     model: '126600LN',
-    //     price: '$20,000',
-    //     condition: 'Good',
-    //     location: 'USA',
-    //     created_at: new Date(),
-    // },
-    // {
-    //     name: 'Rolex Submariner',
-    //     model: '126600LN',
-    //     price: '$20,000',
-    //     condition: 'Good',
-    //     location: 'USA',
-    //     created_at: new Date(),
-    // },
-    // {
-    //     name: 'Rolex Submariner',
-    //     model: '126600LN',
-    //     price: '$20,000',
-    //     condition: 'Good',
-    //     location: 'USA',
-    //     created_at: new Date(),
-    // },
+    {
+        name: 'Rolex Daytona',
+        model: '116500LN',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex GMT Master II',
+        model: '126710BLRO',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex Explorer II',
+        model: '226570',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex Skydweller',
+        model: '326934',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex Oyster Perpetual 41',
+        model: '124300',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex DateJust 41',
+        model: '126333',
+        created_at: new Date(),
+    },
+    {
+        name: 'Rolex Day-Date 40',
+        model: '228236',
+        created_at: new Date(),
+    },
 ];
 
 const seedWatch = () => Watch.bulkCreate(watchData);
