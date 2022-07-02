43fce8e2a245195db7659c9a83fa2f646d9c189d
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-07-01 21:57:04 -0700

    testing heroku app

--- server.js
+++ server.js
@@ -7,38 +7,38 @@ const helpers = require('./utils/helpers');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8889;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// // // Import express-session
-// // const exphbs = require('express-handlebars');
-// // const hbs = exphbs.create({ helpers });
-// // const session = require('express-session');
+// Import express-session
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
 
-// // Set up sessions
-// const sess = {
-//   secret: 'watchesonsale',
-//   cookie: {
-//         // Session will automatically expire in 5 minutes
-//         expires: 30000
-//   },
-//   resave: true,
-//   rolling: true,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   }),
-// };
+// Set up sessions
+const sess = {
+  secret: 'watchesonsale',
+  cookie: {
+        // Session will automatically expire in 5 minutes
+        expires: 30000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
