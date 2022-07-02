70242f8ae7588aad665e29d8218cd6bd96467a35
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-07-01 22:49:15 -0700

    testing cleardb url for heroku

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ const Sequelize = require('sequelize');
 
 // Create a connection to our db, create a ".env" file for your MySQL information 
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.CLEARDB_DATABASE_URL
+  ? new Sequelize(process.env.CLEARDB_DATABASE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
