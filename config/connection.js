9cbb098239d6aa1f4bbddb7abd5651b3e3f3dae9
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-07-01 22:19:53 -0700

    connection error for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      // dialectOptions: {
+      //   decimalNumbers: true,
+      // },
       port: process.env.PORT || 8889
     });
 
