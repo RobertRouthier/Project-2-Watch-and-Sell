fe9cf89cce0c2c770c7c21852a67d2c0e5fff0df
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-07-05 17:33:08 -0700

    edited imgs

--- seeds/watchSeeds.js
+++ seeds/watchSeeds.js
@@ -15,36 +15,7 @@ const watchData = [
         model: '116500LN',
         created_at: new Date(),
     },
-    {
-        name: 'Rolex GMT Master II',
-        model: '126710BLRO',
-        created_at: new Date(),
-    },
-    {
-        name: 'Rolex Explorer II',
-        model: '226570',
-        created_at: new Date(),
-    },
-    {
-        name: 'Rolex Skydweller',
-        model: '326934',
-        created_at: new Date(),
-    },
-    {
-        name: 'Rolex Oyster Perpetual 41',
-        model: '124300',
-        created_at: new Date(),
-    },
-    {
-        name: 'Rolex DateJust 41',
-        model: '126333',
-        created_at: new Date(),
-    },
-    {
-        name: 'Rolex Day-Date 40',
-        model: '228236',
-        created_at: new Date(),
-    },
+   
 ];
 
 const seedWatch = () => Watch.bulkCreate(watchData);
